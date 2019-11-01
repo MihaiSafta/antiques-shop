476dc97689cf80654d24c366fd1925fdc73f9368
ShinnoK <mihaisaftas.259@gmail.com>
2019-11-01 13:57:05 +0200

    update

--- src/test/java/org/fasttrackit/antiquesshop/ProductServiceIntegrationTest.java
+++ src/test/java/org/fasttrackit/antiquesshop/ProductServiceIntegrationTest.java
@@ -1,14 +1,15 @@
 package org.fasttrackit.antiquesshop;
-
+import org.junit.Test;
 import org.fasttrackit.antiquesshop.domain.Product;
+import org.fasttrackit.antiquesshop.exception.ResourceNotFoundException;
 import org.fasttrackit.antiquesshop.service.ProductService;
 import org.fasttrackit.antiquesshop.transfer.product.CreateProductRequest;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.TransactionSystemException;
+
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -23,6 +24,38 @@ public class ProductServiceIntegrationTest {
     @Test
     public void testCreateProduct_whenValidRequest_thenReturnCreatedProduct(){
 
+        createProduct();
+    }
+
+
+
+    @Test(expected = TransactionSystemException.class)
+    public  void testCreateProduct_whenInvalidRequest_thenThrowException(){
+
+        CreateProductRequest request = new CreateProductRequest();
+
+        productService.createProduct(request);
+    }
+    @Test
+    public void testGetProductById_whenExistingEntity_thenReturnProduct(){
+        Product createdProduct = createProduct();
+        Product retrievedProduct = productService.getProduct(createdProduct.getId());
+        assertThat(retrievedProduct, notNullValue());
+        assertThat(retrievedProduct.getId(), is(createdProduct.getId()));
+        assertThat(retrievedProduct.getName(), is(createdProduct.getName()));
+
+    }
+    @Test(expected = ResourceNotFoundException.class)
+    public void testGetProduct_whenNonExistingEntity_thenTrowNotFoundException(){
+        productService.getProduct(9999);
+    }
+
+
+
+
+
+
+    private Product createProduct() {
         CreateProductRequest request = new CreateProductRequest();
         request.setName("Omega");
         request.setDescription("Pocket Watch");
@@ -37,17 +70,6 @@ public class ProductServiceIntegrationTest {
         assertThat(product.getDescription(), is(request.getDescription()));
         assertThat(product.getQuantity(), is(request.getQuantity()));
         assertThat(product.getPrice(), is(request.getPrice()));
-
-
+        return product;
     }
-
-    @Test()
-    public  void testCreateProduct_whenInvalidRequest_thenThrowException(){
-
-        CreateProductRequest request = new CreateProductRequest();
-
-        productService.createProduct(request);
-    }
-
-
 }
