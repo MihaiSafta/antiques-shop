19b6b841880617180e5304ffbe77b70c08d8ef6b
ShinnoK <mihaisaftas.259@gmail.com>
2019-11-11 12:19:01 +0200

    update 12.18

--- src/main/java/org/fasttrackit/antiquesshop/domain/Product.java
+++ src/main/java/org/fasttrackit/antiquesshop/domain/Product.java
@@ -3,7 +3,10 @@ package org.fasttrackit.antiquesshop.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 
@@ -11,13 +14,18 @@ public class Product {
     @Id
     @GeneratedValue
     private long id;
+
     @NotNull
     private  String name;
     private int quantity;
+
     @NotNull
     private double price;
     private String description;
     private String imagePath;
+
+    @ManyToMany(mappedBy = "products")
+    private Set<Cart> carts = new HashSet<>();
 
     public long getId() {
         return id;
@@ -66,7 +74,13 @@ public class Product {
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
     }
+    public Set<Cart> getCarts() {
+        return carts;
+    }
 
+    public void setCarts(Set<Cart> carts) {
+        this.carts = carts;
+    }
     @Override
     public String toString() {
         return "Product{" +
@@ -77,5 +91,21 @@ public class Product {
                 ", description='" + description + '\'' +
                 ", imagePath='" + imagePath + '\'' +
                 '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return id == product.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
     }
 }
--- src/test/java/org/fasttrackit/antiquesshop/CartServiceIntegrationTest.java
+++ src/test/java/org/fasttrackit/antiquesshop/CartServiceIntegrationTest.java
@@ -1,6 +1,10 @@
 package org.fasttrackit.antiquesshop;
 
+import org.fasttrackit.antiquesshop.domain.Customer;
 import org.fasttrackit.antiquesshop.service.CartService;
+import org.fasttrackit.antiquesshop.steps.CustomerSteps;
+import org.fasttrackit.antiquesshop.transfer.cart.AddProductToCartRequest;
+import org.fasttrackit.antiquesshop.transfer.product.CreateProductRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +17,18 @@ public class CartServiceIntegrationTest {
 
     @Autowired
     private CartService cartService;
-    @Test
-    public void testAddToCart_WhenNewCart_thenCreateNewCart()   {
+    @Autowired
+    private CustomerSteps customerSteps;
 
+
+    @Test
+    public void testAddToCart_whenNewCart_thenCreateNewCart()   {
+        Customer customer = customerSteps.createCustomer();
+
+        AddProductToCartRequest request = new AddProductToCartRequest();
+        request.setCustomerId(customer.getId());
+        request.setProductId(10L);
+        cartService.addProductToCart(request);
 
     }
 
